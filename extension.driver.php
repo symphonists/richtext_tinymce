cda1a72c5430c35ca84eacda23e9c21fb38afd5b
Nick Dunn <nick@nick-dunn.co.uk>
2012-03-16 18:59:26 +0000

    Updated to be compatible with S2.3

--- extension.driver.php
+++ extension.driver.php
@@ -1,40 +1,29 @@
 <?php
 
 	Class extension_richtext_tinymce extends Extension{
-
-		public function about(){
-			return array('name' => 'Formatter: Rich Text (TinyMCE)',
-						 'version' => '2.0',
-						 'release-date' => '2011-03-22',
-						 'author' => array('name' => 'Symphony Community',
-										   'website' => 'https://github.com/symphonists')
-				 		);
-		}
 		
 		public function getSubscribedDelegates(){
 			return array(
-						array(
-							'page' => '/backend/',
-							'delegate' => 'ModifyTextareaFieldPublishWidget',
-							'callback' => 'modifyTextarea'
-						),
-						
+				array(
+					'page'		=> '/backend/',
+					'delegate'	=> 'InitaliseAdminPageHead',
+					'callback'	=> 'initaliseAdminPageHead'
+				)
 			);
 		}
 		
-		public function modifyTextarea($context){
-			if($context['field']->get('formatter') != 'tinymce') return;
+		public function initaliseAdminPageHead($context) {
+			$page = Administration::instance()->Page;
 			
-			if(!defined('__TINYMCE_SCRIPTS_IN_HEAD__') || !__TINYMCE_SCRIPTS_IN_HEAD__){
-				define_safe('__TINYMCE_SCRIPTS_IN_HEAD__', TRUE);
-				Administration::instance()->Page->addScriptToHead(URL . '/extensions/richtext_tinymce/lib/jquery.tinymce.js', 200);
-				Administration::instance()->Page->addScriptToHead(URL . '/extensions/richtext_tinymce/assets/richtext_tinymce.publish.js', 201);
-			}
+			// only on publish pages
+			if(!$page instanceOf contentPublish) return;
 			
-			$context['textarea']->setAttribute('id', trim($context['textarea']->getAttribute('id') . ' ' . $context['field']->get('element_name')));
-				
+			// which are showing new/edit form
+			$callback = Administration::instance()->getPageCallback();
+			if(!in_array($callback['context']['page'], array('new', 'edit'))) return;
+			
+			Administration::instance()->Page->addScriptToHead(URL . '/extensions/richtext_tinymce/lib/jquery.tinymce.js', 200);
+			Administration::instance()->Page->addScriptToHead(URL . '/extensions/richtext_tinymce/assets/richtext_tinymce.publish.js', 201);
 		}
 		
-		
 	}
-
